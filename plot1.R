ab73bcd0acbb08e351f09584cb45bc98df2f2b5b
Andrea Bisognin <andrea.bisognin@gmail.com>
2014-08-11 18:16:31 +0200

    added plot1 code

---
+++ plot1.R
@@ -0,0 +1,16 @@
+SCC <- readRDS("Source_Classification_Code.rds")
+NEI <- readRDS("summarySCC_PM25.rds")
+
+nei.list <- split(NEI,NEI$year)
+
+sapply(nei.list,nrow)
+emi.data <- lapply(nei.list, function(x)x$Emissions)
+logged.emi.data <- lapply(emi.data,log10)
+
+png("plot1.png")
+title = expression(paste("Global emissions of ",PM[2.5]," from 1999 to 2008"))
+boxplot(logged.emi.data,main=title,xlab="year",
+        ylab=expression(log[10](Emissions)),notch=TRUE,outline=FALSE)
+dev.off()
+
+
